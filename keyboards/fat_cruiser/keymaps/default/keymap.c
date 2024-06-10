1dfab408267cb6e7b5114c6f54f8b6968296c9c0
Marcus Östling <mpt.ostling@gmail.com>
2024-06-10 11:12:26 +0200

    change shift and tab

--- keyboards/fat_cruiser/keymaps/default/keymap.c
+++ keyboards/fat_cruiser/keymaps/default/keymap.c
@@ -168,10 +168,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,  KC_NO, KC_NO, /*,*/  KC_NO,   KC_NO, KC_NO
     ),
     [_GAMING] = LAYOUT_ortho_36(
-        KC_LSFT, KC_Q,   KC_W,   KC_E,    KC_R,   /*,*/   KC_Y, KC_U, KC_I,    KC_O,   KC_P,
+        KC_ESC, KC_Q,   KC_W,   KC_E,    KC_R,   /*,*/   KC_Y, KC_U, KC_I,    KC_O,   KC_P,
         KC_LCTL, KC_A,   KC_S,   KC_D,    KC_F,   /*,*/   KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,
         KC_TAB, KC_Z,   KC_X,   KC_C,    KC_V,   /*,*/   KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
-        KC_ESC, OSL(_GAMING_UTIL) , KC_SPC, /*,*/ KC_ENT, KC_TAB, TO(_QWERTY)
+        KC_LSFT, OSL(_GAMING_UTIL) , KC_SPC, /*,*/ KC_ENT, KC_TAB, TO(_QWERTY)
     ),
     [_GAMING_UTIL] = LAYOUT_ortho_36(
         KC_ASTR, KC_7,    KC_8,    KC_9, KC_EQUAL, /*,*/  KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,
